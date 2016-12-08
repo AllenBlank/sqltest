7a341ac21c802644f384ee700b1d20d409aece06
Allen Blank <zuosoft@yahoo.com>
2016-12-08 13:30:37 -0800

    editing views, routes, controllers to simplify

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :carriers
+  resources :orders, only: [:show, :index]
+  resources :carriers, only: [:show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
